eab51400e49e1efe1f48aaf4b859065daed102db
DavidDock <ddocksey@live.co.uk>
2024-03-27 11:25:12 +0000

    Add basic html and css structure to footer

--- src/components/FooterNav.js
+++ src/components/FooterNav.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import styles from "../styles/FooterNav.module.css"
+import boarderStyles from "../styles/Boarders.module.css"
 
 const FooterNav = () => {
   return (
-    <p>Footer </p>
+    <footer className={`mt-2 ${styles.Footer}`}>
+      <div className={`m-2 ${styles.Logo}`}><b>Tha<span>nks</span>⁴Vi<span>sit</span>ing</b>
+      </div>
+      <div className={styles.FooterNavLinkContainer}>
+        <a className={`mr-4 mb-2 ${boarderStyles.NormalBoarder}`}>Contact</a>
+        <a className={`mr-4 mb-2 ${boarderStyles.NormalBoarder}`}>Register</a>
+      </div>
+    </footer>
   )
 }
 
