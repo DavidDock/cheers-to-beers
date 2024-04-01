5f91bd16c8819687489d608292419122bb4ed526
DavidDock <ddocksey@live.co.uk>
2024-04-01 23:36:27 +0000

    Add currentUser context to footerNav and display correct links

--- src/components/FooterNav.js
+++ src/components/FooterNav.js
@@ -2,30 +2,51 @@ import React from 'react';
 import styles from "../styles/FooterNav.module.css";
 import boarderStyles from "../styles/Boarders.module.css";
 import { NavLink } from "react-router-dom";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
 
 const FooterNav = () => {
+
+  const currentUser = useCurrentUser();
+
+    const loggedInIcons = (
+        <>
+            <NavLink
+                exact
+                className={`mr-4 mt-2 ${boarderStyles.NormalBoarder}`}
+                activeClassName={boarderStyles.Active}
+                to="/contact"
+            >
+                Contact
+            </NavLink>
+            <NavLink
+                exact
+                className={`mr-4 mt-2 ${boarderStyles.NormalBoarder}`}
+                activeClassName={boarderStyles.Active}
+                to="/"
+            >
+                Logout
+            </NavLink>
+        </>
+    );
+    const loggedOutIcons = (
+        <>
+            <NavLink
+                exact
+                className={`mr-4 mt-2 ${boarderStyles.NormalBoarder}`}
+                activeClassName={boarderStyles.Active}
+                to="/signup"
+            >
+                Register
+            </NavLink>
+        </>
+    );
+  
   return (
     <footer className={`mt-2 ${styles.Footer}`}>
       <div className={`m-2 ${styles.Logo}`} aria-label='Thanks For Visiting Logo'><b>Tha<span>nks</span>⁴Vi<span>sit</span>ing</b>
       </div>
       <div className={styles.FooterNavLinkContainer}>
-        <NavLink
-          exact
-          className={`mr-4 mt-2 ${boarderStyles.NormalBoarder}`}
-          activeClassName={boarderStyles.Active}
-          to="/contact"
-        >
-          Contact
-        </NavLink>
-        <NavLink
-          exact
-          className={`mr-4 mt-2 ${boarderStyles.NormalBoarder}`}
-          activeClassName={boarderStyles.Active}
-          to="/signup"
-        >
-          Register
-        </NavLink>
-
+      {currentUser ? loggedInIcons : loggedOutIcons}
       </div>
     </footer>
   )
