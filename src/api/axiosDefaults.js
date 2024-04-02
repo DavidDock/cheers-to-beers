be77016be1fb6d990be38dbe84f5e63ea510929a
DavidDock <ddocksey@live.co.uk>
2024-04-02 20:28:46 +0000

    Add axios intercptors access and refresh tokens in currentUser context

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://cheers-to-beers-api-5a858b4697bd.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
