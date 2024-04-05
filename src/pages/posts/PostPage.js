d32df99baeac236e2833a1e318aac876c3990f8b
DavidDock <ddocksey@live.co.uk>
2024-04-05 18:50:10 +0000

    Add functionality to set a post in PostPage

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 function PostPage() {
-  // Add your logic here
+  const { id } = useParams();
+  const [post, setPost] = useState({ results: [] });
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: post }] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+        ]);
+        setPost({ results: [post] });
+        console.log(post);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [id]);
 
 
   return (
-    <Row className="h-100">
-      <Col className="py-2 p-0 p-lg-2" lg={8}>
+    <Row className="">
+      <Col className="" lg={8}>
         <p>Post component</p>
         <Container className="">
           Comments
