0181e6e3d84893f78e2a619582f7b77ab45d68cc
DavidDock <ddocksey@live.co.uk>
2024-04-12 15:18:10 +0000

    Add search bar and functionality to PostsPage

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 
-import { Container, Col, Row } from "react-bootstrap";
+import { Form, Container, Col, Row } from "react-bootstrap";
 import styles from "../../styles/PostsPage.module.css";
+import borderStyles from "../../styles/Borders.module.css";
 
 import { useLocation } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
@@ -14,12 +15,13 @@ function PostsPage({ message, filter = "" }) {
     const [posts, setPosts] = useState({ results: [] });
     const [hasLoaded, setHasLoaded] = useState(false);
     const { pathname } = useLocation();
+    const [query, setQuery] = useState("");
 
 useEffect(() => {
     // fetch posts depending on filter and set
     const fetchPosts = async () => {
       try {
-        const { data } = await axiosReq.get(`/posts/?${filter}`);
+        const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
         setPosts(data);
         setHasLoaded(true);
       } catch (err) {
@@ -28,12 +30,34 @@ useEffect(() => {
     };
 
     setHasLoaded(false);
-    fetchPosts();
-  }, [filter, pathname]);
+    const timer = setTimeout(() => {
+        fetchPosts();
+      }, 750);
+      return () => {
+        clearTimeout(timer);
+      };
+
+  }, [filter, query, pathname]);
   
   return (
     <Row className="d-flex justify-content-center">
         <Col className= "mx-4 mx-md-5 my-4" lg={7}>
+
+            {/* Search  bar*/}
+            <i className={`fas fa-search ${styles.SearchIcon}`} />
+            <Form
+                className={`mx-auto mb-4 ${styles.SearchBar}`}
+                onSubmit={(event) => event.preventDefault()}
+            >
+                <Form.Control
+                value={query}
+                onChange={(event) => setQuery(event.target.value)}
+                type="text"
+                className={`mr-sm-2 ${borderStyles.Input}`}
+                placeholder="Search posts"
+                />
+            </Form>
+
             {/* Display posts or spinner*/}
             {hasLoaded ? (
                 <>
