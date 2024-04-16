ab42fd6993d1c8db49ab7fee54cb3ee65d6dac18
DavidDock <ddocksey@live.co.uk>
2024-04-16 10:49:22 +0000

    Add edit comment form with funtionality and add to post page

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Media } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 import Avatar from "../../components/Avatar";
 import { MoreDropdown } from "../../components/MoreDropdown";
+import CommentEditForm from "./CommentEditForm";
 
 import styles from "../../styles/Comment.module.css";
 
@@ -22,6 +23,8 @@ const Comment = (props) => {
         setPost,
         setComments,
     } = props;
+
+    const [showEditForm, setShowEditForm] = useState(false);
 
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === owner;
@@ -56,10 +59,24 @@ const Comment = (props) => {
         <Media.Body className="text-left ml-2">
           <span className={styles.Owner}>{owner}</span>
           <span className={styles.Date}>{updated_at}</span>
-          <p>{content}</p>
+          {showEditForm ? (
+            <CommentEditForm
+                id={id}
+                profile_id={profile_id}
+                content={content}
+                profileImage={profile_image}
+                setComments={setComments}
+                setShowEditForm={setShowEditForm}
+            />
+          ) : (
+            <p>{content}</p>
+          )}
         </Media.Body>
-        {is_owner && (
-          <MoreDropdown handleEdit={() => {}} handleDelete={handleDelete} />
+        {is_owner && !showEditForm && (
+          <MoreDropdown
+            handleEdit={() => setShowEditForm(true)}
+            handleDelete={handleDelete}
+          />
         )}
       </Media>
     </div>
