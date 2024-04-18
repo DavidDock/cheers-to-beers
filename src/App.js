3cadc52ad6e5aedef0f792a9cea81384eebe7788
DavidDock <ddocksey@live.co.uk>
2024-04-18 11:55:16 +0000

    Add routes for edit username and password

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ import PostPage from "./pages/posts/PostPage";
 import PostsPage from "./pages/posts/PostsPage";
 import ProfilePage from "./pages/profiles/ProfilePage";
 import ProfileEditForm from "./pages/profiles/ProfileEditForm";
+import UsernameForm from "./pages/profiles/UsernameForm";
+import UserPasswordForm from "./pages/profiles/UserPasswordForm";
 
 import { useCurrentUser } from "./contexts/CurrentUserContext";
 
@@ -78,6 +80,8 @@ function App() {
               <Route exact path="/posts/:id" render={() => <PostPage />} />
               <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
               <Route exact path="/profiles/:id/edit" render={() => <ProfileEditForm />} />
+              <Route exact path="/profiles/:id/edit/username" render={() => <UsernameForm />} />
+              <Route exact path="/profiles/:id/edit/password" render={() => <UserPasswordForm />} />
               <Route render={() => <p>Page not found!</p>} />
             </Switch>
           </Container>
